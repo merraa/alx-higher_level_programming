1001bf08cdc57e6907aa94164065bf64377579f5
merraa <meron12345@yahoo.com>
2022-08-29 14:23:26 -0700

    kl

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     count = len(my_list)
-    if (idx > (count - 1)) or (idx < 0:)
+    if (idx > (count - 1)) or (idx < 0):
         return None
     else:
         return (mylist[idx])
