d7d4bb1e3227d5b99cf32a6f04c649091e77e992
meron <104758118+merraa@users.noreply.github.com>
2022-12-16 00:48:15 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #5
-curl -sX POST -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' $1
+curl -sX POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD' $1
