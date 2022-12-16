cfbe964b89dca41fb2a8f3320831e844aaa77e35
meron <104758118+merraa@users.noreply.github.com>
2022-12-16 21:45:18 +0300

    Delete 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Use the package request"""
-if __name__ == "__main__":
-    import requests
-
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
