c0b21394c7020fcb0445f4a1336891580165c7f2
meron <104758118+merraa@users.noreply.github.com>
2022-12-16 21:46:50 +0300

    Add files via upload

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""#4"""
+import requests
+
+
+def myFirstRequest():
+    """My requests"""
+    x = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(x.text)))
+    print("\t- content: {}".format(x.text))
+
+if __name__ == "__main__":
+    myFirstRequest()
